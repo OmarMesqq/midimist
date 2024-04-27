5f37d4c59c1e05e121057d40d50e2ee000be3b0e
OmarMesqq <omarmsqt@gmail.com>
2024-04-27 19:59:56 -0300

    refactor: :sparkles: melhora stack de navegação da top nav bar

--- mobile_frontend/src/components/commons/TopBar.tsx
+++ mobile_frontend/src/components/commons/TopBar.tsx
@@ -1,9 +1,31 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, View, TouchableOpacity } from "react-native";
+import { useNavigation } from '@react-navigation/native';
 
 export const TopBar = () => {
+  const navigation = useNavigation();
+
+  const navigateToNotifications = () => {
+    navigation.navigate('Notifications');
+  };
+
+  const navigateToProfile = () => {
+    navigation.navigate('Profile');
+  };
+
   return (
     <View style={styles.headerContainer}>
-	<Image source={require('../../assets/images/logo.png')} style={styles.logo} />
+      <Image source={require('../../assets/images/logo.png')} style={styles.logo} />
+
+      <View style={styles.rightContainer}>
+        <TouchableOpacity onPress={navigateToNotifications}>
+          <Image source={require('../../assets/images/notifications.png')} style={styles.icon} />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={navigateToProfile}>
+          <Image source={require('../../assets/images/profile.png')} style={styles.icon} />
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 };
@@ -14,13 +36,24 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
     width: '100%',
-    backgroundColor: '#000000', // Set the background color as needed
+    backgroundColor: '#000000',
   },
   logo: {
-    width: 60,
+    width: 40,
     height: 40,
     resizeMode: 'contain',
+  },
+  rightContainer: {
+    flexDirection: 'row', // Keep the notification and profile icons side by side
+    alignItems: 'center',
+  },
+  icon: {
+    width: 40,
+    height: 40,
+    resizeMode: 'contain',
+    marginLeft: 15,
+    marginRight: 5,
   }
 });
-
