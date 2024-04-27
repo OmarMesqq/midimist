ab4690b8575de0c4315d5f5bfae1d58eca6637e7
OmarMesqq <omarmsqt@gmail.com>
2024-04-27 19:59:18 -0300

    refactor: :lipstick: faz o fundo da tela ser preto nas experiencias principais

--- mobile_frontend/src/components/layout/Home.tsx
+++ mobile_frontend/src/components/layout/Home.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export const Home = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#ffffff' }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#000000' }}>
       <Text>Home Screen</Text>
     </View>
   );
