033262899f71efa02f28280fc2ee0027107644d1
OmarMesqq <omarmsqt@gmail.com>
2024-05-12 21:26:06 -0300

    chore: :sparkles: adiciona estilos globais do projeto locofy

---
+++ mobile_frontend/src/GlobalStyles.js
@@ -0,0 +1,47 @@
+/* fonts */
+export const FontFamily = {
+  interBold: "Inter-Bold",
+  interLight: "Inter-Light",
+  interSemiBold: "Inter-SemiBold",
+  interExtraLight: "Inter-ExtraLight",
+  interRegular: "Inter-Regular",
+};
+/* font sizes */
+export const FontSize = {
+  size_smi: 13,
+  size_xl: 20,
+  size_base: 16,
+  size_xs: 12,
+  size_mini: 15,
+  size_11xl: 30,
+  size_3xs: 10,
+};
+/* Colors */
+export const Color = {
+  colorBlack: "#000",
+  colorGray_100: "#1d1d1d",
+  colorGray_200: "rgba(0, 0, 0, 0.3)",
+  colorWhite: "#fff",
+  colorGainsboro: "#d9d9d9",
+  colorBlueviolet: "#9747ff",
+  colorDimgray: "#707070",
+  colorDarkslategray: "#464646",
+};
+/* Paddings */
+export const Padding = {
+  p_5xs: 8,
+  p_31xl: 50,
+  p_11xl: 30,
+  p_xl: 20,
+  p_8xs: 5,
+  p_3xs: 10,
+  p_11xs: 2,
+  p_9xs: 4,
+};
+/* border radiuses */
+export const Border = {
+  br_3xs: 10,
+  br_xl: 20,
+  br_8xs: 5,
+  br_8xl: 27,
+};
