e1299f5b596de1e851abb0aedab6deb97bb2825b
Omar Mesquita <omar.mesquita@calindra.com.br>
2024-06-05 14:40:49 -0300

    refactor: :recycle: usa novo pacote para criar carrossel

--- mobile_frontend/src/components/commons/Carrossel.jsx
+++ mobile_frontend/src/components/commons/Carrossel.jsx
@@ -1,52 +1,50 @@
-import React from "react";
+import React from 'react';
 import { View, Image, StyleSheet, Dimensions } from 'react-native';
-import Carousel from "react-native-snap-carousel";
-//import { get_instance, instance_type } from "../../api/instance";
-//import { get_movie_details, get_movie_id } from "../../api_handler/api_handler"
+import Carousel from 'react-native-reanimated-carousel';
 
-const {width: viewPortWidth} = Dimensions.get('window');
+const { width: viewPortWidth } = Dimensions.get('window');
 
 const Carrossel = ({ data }) => {
-    const renderItem = ({ item }) => (
-      <View style={styles.slide}>
+  const renderItem = ({ item }) => (
+    <View style={styles.slide}>
       {item && item.poster_path ? (
-      <Image
-        source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }}
-        style={styles.poster}
-      />
-    ) : null}
-  </View>
-);
-  
-    return (
-      <Carousel
-        data={data}
-        renderItem={renderItem}
-        sliderWidth={viewPortWidth}
-        itemWidth={viewPortWidth * 0.75}
-        loop={true}
-        autoplay={true}
-      />
-    );
-  };
-  
-  const styles = StyleSheet.create({
-    slide: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    poster: {
-      width: '100%',
-      height: 200, 
-      borderRadius: 10,
-      marginBottom: 10,
-    },
-    title: {
-      fontSize: 18,
-      fontWeight: 'bold',
-      textAlign: 'center',
-    },
-  });
-  
-  export default Carrossel;
+        <Image
+          source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }}
+          style={styles.poster}
+        />
+      ) : null}
+    </View>
+  );
+
+  return (
+    <Carousel
+      width={viewPortWidth}
+      height={200}
+      data={data}
+      renderItem={renderItem}
+      loop={true}
+      autoPlay={true}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  poster: {
+    width: '100%',
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default Carrossel;
